e89074a2730d037d15c2033f5994cf06fa209fa2
Adam Waterman <awaterman5895@gmail.com>
2019-09-30 19:29:24 +0100

    get next pass

--- test.py
+++ test.py
@@ -2,8 +2,15 @@
 
 # pylint:disable=all
 
+import datetime
 import requests
+from pprint import pprint
 
 ip_data = requests.get('https://ipinfo.io').json()
 lat, lon = ip_data['loc'].split(',')
 print(lat, lon)
+
+pass_data = requests.get(
+    'http://api.open-notify.org/iss-pass.json?lat={}&lon={}'.format(lat, lon)).json()
+pprint(pass_data)
+
