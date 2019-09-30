d64faf7f57aed62c9a3d48c90ef11ddc0cf281c9
Robin Smith <robincsmith84@gmail.com>
2019-09-30 20:21:14 +0100

    Post a tweet

---
+++ credentials_safe.py
@@ -0,0 +1,6 @@
+# Add your own secrets here
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
---
+++ post_tweet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import twitter
+from hidden import consumer_key, consumer_secret, access_token, access_secret
+
+api = twitter.Api(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token_key=access_token,
+    access_token_secret=access_secret
+)
+
+message = 'Some text'
+
+status = api.PostUpdate(message)
+print(status.text)
